638f004ed6456aae1474831fbef2f1582e6b5fd5
Nitin Kumar <kumar.nk.nitin77@email.com>
2026-06-30 06:41:47 +0000

    fixed deprecated warnings

--- dev/locals.tf
+++ dev/locals.tf
@@ -7,7 +7,7 @@ locals {
 
   # Account information
   account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
+  region     = data.aws_region.current.region
 
   # Common tags
   common_tags = merge(
